9419a453f4a058f98e613264526fcc5371d7c00f
Khaiqol <Khaiqol@gmail.com>
2025-05-07 15:58:21 +0700

    Tampilkan tanggal pada setiap item todo

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -143,6 +143,10 @@ class TodoPage extends StatelessWidget {
                                       ),
                                     ),
                                     SizedBox(height: 4.0),
+                                    Text(
+                                      '${todo.date.day}/${todo.date.month}/${todo.date.year}',
+                                      style: TextStyle(color: Colors.grey),
+                                    ),
                                   ],
                                 ),
                               ],
